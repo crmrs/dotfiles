fb102890ff54c355518d5135e2335e666d596e6c
carm4ris <crmrs@proton.me>
2025-02-26 18:52:16 +0100

    feat: Add `fuzzel-niri-windows` script and shortcut

--- scripts/fuzzel-niri-windows.sh
+++ scripts/fuzzel-niri-windows.sh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
 WINDOW=$(niri msg -j windows | jq -c '.[].title' | tr -d '"' | fuzzel -d);
-echo $WINDOW; 
 
 ID=$(niri msg -j windows | jq --arg window $WINDOW -c '.[] | select(.title == $window).id');
-echo $ID;
 
 niri msg action focus-window --id $ID
